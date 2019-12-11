3fed0d3a68a2f0497af6940efb457ad8171269b4
Christo <christo@cryptodrunks.net>
2019-12-11 13:24:56 +0700

    fix sugarcube format and use json as temporary storage format

--- src/plugins/twitterscraper.js
+++ src/plugins/twitterscraper.js
@@ -1,49 +1,75 @@
 const os = require("os");
-const fs = require("fs");
 const { URL } = require("url");
 const { retry, flatmapP } = require("dashp");
 const { envelope: env } = require("@sugarcube/core");
 const { runCmd } = require("@sugarcube/utils");
 const { promisify } = require("util");
 const { cleanUp, existsP } = require("@sugarcube/plugin-fs");
-const csv = require("csvtojson");
-const { parse } = require("date-fns");
-const readFile = promisify(fs.readFile);
+const { parse, fromUnixTime } = require("date-fns");
+const { once } = require('events');
+const { createReadStream } = require('fs');
+const { createInterface } = require('readline');
 
-const querySource = "twitter_tweet";
+const querySource = "twitter_user";
 
-function parseTweetId(id) {
-  if (id.startsWith("http")) {
-    const u = new URL(id);
-    return u.pathname.split("/").filter(x => x !== "")[2];
+const parseTwitterUser = user => {
+  if (Number.isInteger(user)) return user.toString();
+  if (user.startsWith("http")) {
+    const u = new URL(user);
+    return u.pathname
+      .replace(/^\//, "")
+      .replace(/\/$/, "")
+      .split("/")[0];
   }
-  return id;
-}
+  return user.replace(/^@/, "");
+};
+
+async function processTwintTmpFile(file) {
+  const tweets = [];
+
+  const rl = createInterface({
+    input: createReadStream(file),
+    crlfDelay: Infinity
+  });
+
+  rl.on("line", (line) => {
+    tweets.push(JSON.parse(line));
+  });
+
+
+
+  await once(rl, "close");
+
+  return tweets;
+};
 
 async function twitterScraper(user) {
   const tempDir = os.tmpdir();
-  const csvFile = `${tempDir}/tweets-${user}.csv`;
+  const tmpFile = `${tempDir}/tweets-${user}.csv`;
   let data = [];
 
-  await runCmd("twint", ["-u", user, "-o", csvFile, "--csv"]);
-  if (await existsP(csvFile)) {
-    data = await csv().fromFile(csvFile);
-    await cleanUp(csvFile);
+  await runCmd(
+    "twint",
+    ["-u", user, "--profile-full", "-o", tmpFile, "--json"]
+  );
+
+  if (await existsP(tmpFile)) {
+    data = await processTwintTmpFile(tmpFile);
+    await cleanUp(tmpFile);
   }
   return data;
 }
 
 const plugin = async (envelope, { stats, log }) => {
   const queries = env
-    .queriesByType(querySource, envelope)
-    .map(term => parseTweetId(term));
+    .queriesByType(querySource, envelope);
 
   const data = await flatmapP(async query => {
     stats.count("total");
     let tweets;
     log.info(query);
     try {
-      tweets = await twitterScraper(query);
+      tweets = await twitterScraper(parseTwitterUser(query));
     } catch (e) {
       log.error(`Failed to scrape ${query}: ${e.message}`);
       stats.fail({ term: query, reason: e.message });
@@ -53,43 +79,57 @@ const plugin = async (envelope, { stats, log }) => {
     stats.count("success");
 
     log.info(`We have scraped ${tweets.length} tweets for ${query}.`);
+
     return tweets.map(
       ({
         id,
-        date,
-        time,
-        username,
         name,
+        username,
+        user_id,
+        created_at,
         tweet,
-        urls,
         photos,
-        hashtags,
-        link,
         video,
+        hashtags,
+        link: href,
+        retweets_count,
+        likes_count,
+        lang,
+        place,
         geo,
-        place
       }) => {
-        const timestamp = parse(
-          `${date}:${time}`,
-          "yyyy-MM-dd:H:mm:ss:SS",
-          new Date()
-        );
-        return {
-          _sc_id_fields: [id],
-          _sc_content_fields: [tweet],
-          _sc_media: [...photos],
-          timestamp,
-          time,
-          username,
+        const images = photos.map((url) => ({type: "image", term: url}));
+        const videos = video === 1 ? [{type: "video", term: href}] : [];
+        const urls = [{type: "url", term: href}];
+        const createdAt = fromUnixTime(created_at/1000);
+
+        const user = {
           name,
+          screen_name: username,
+          user_id: user_id.toString(),
+        };
+
+        return {
+          _sc_id_fields: ["tweet_id"],
+          _sc_content_fields: ["tweet"],
+          _sc_media: images.concat(videos).concat(urls),
+          _sc_pubdates: {
+            source: createdAt,
+          },
+          _sc_queries: [
+            {type: querySource, term: query},
+          ],
+          tweet_id: id.toString(),
+          tweet_time: createdAt,
+          geo: geo === "" ? null : geo,
+          place: place === "" ? null : place,
+          lang: lang === "" ? null : lang,
+          hashtags: hashtags.map((tag) => ({tag: tag.toLowerCase(), original_tag: tag.replace(/^#/, "")})),
           tweet,
-          urls,
-          photos,
-          hashtags,
-          link,
-          video,
-          geo,
-          place
+          href,
+          retweet_count: retweets_count == null ? 0 : retweets_count,
+          favorite_count: likes_count == null ? 0 : likes_count,
+          user,
         };
       }
     );
